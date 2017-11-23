0bcc42fc665371724ae05930b5ade4ce08cc6d2d
Damar Rotama <damar.rotama@gmail.com>
2017-11-23 15:26:32 +0700

    first commit

---
+++ app/src/main/java/com/ghifa/mobile/kelompokproyek/IndexActivity.java
@@ -0,0 +1,18 @@
+package com.ghifa.mobile.kelompokproyek;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Mobile on 11/23/2017.
+ */
+
+public class IndexActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_index);
+
+    }
+}
